14c518d326129a12ba533bb3f6e2135a927399c6
nightvoyager111 <xianghanzzz@outlook.com>
2025-08-03 13:03:11 -0400

    Refactor the webcam model code into a callable function and create a FastAPI backend

---
+++ app_backend/model_inference.py
@@ -0,0 +1,40 @@
+import torch
+from PIL import Image
+from torchvision import transforms
+import sys, os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from primary_model.ame_try1 import EmotionalAlextNet
+
+model = EmotionalAlextNet(num_classes=7, use_residual=True)
+checkpoint = torch.load('./models/model_EmotionAlexNet_bs64_lr0.0005_epoch18_20250707_151009.pt', map_location=torch.device('cpu'))
+model.load_state_dict(checkpoint)
+model.eval()
+
+EMOTION_MAPPING = {
+    'anger': 'ANGRY',
+    'disgust': 'DISGUSTED',
+    'fear': 'FEARFUL',
+    'happiness': 'HAPPY',
+    'neutral': 'NEUTRAL',
+    'sadness': 'SAD',
+    'surprise': 'SURPRISED'
+}
+
+# Preprocessing (match training)
+transform = transforms.Compose([
+    #transforms.Grayscale(),
+    transforms.Resize((48, 48)),
+    transforms.ToTensor()
+])
+
+def predict_emotion(pil_img):
+    img = pil_img.convert('RGB')
+    tensor = transform(img).unsqueeze(0)  
+    with torch.no_grad():
+        output = model(tensor)
+        probs = torch.exp(output)
+        pred_idx = torch.argmax(probs, dim=1).item()
+        emotion = list(EMOTION_MAPPING.keys())[pred_idx]
+        return emotion
+    
